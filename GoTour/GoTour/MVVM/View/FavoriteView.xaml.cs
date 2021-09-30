a4805b8e701e15f951f60917f0f3e080e4459795
TriThuc2321 <19522321@gm.uit.edu.vn>
2021-09-30 14:38:39 +0700

    a

--- GoTour/GoTour/MVVM/View/FavoriteView.xaml.cs
+++ GoTour/GoTour/MVVM/View/FavoriteView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoTour.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace GoTour.MVVM.View
         public FavoriteView()
         {
             InitializeComponent();
+            this.BindingContext = new FavoriteViewModel(Navigation);
         }
     }
 }
