ca8762117ef115bef7f4c409243cac842649f7fb
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-10-04 01:05:58 +0700

    ok

--- GoTour/GoTour.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ GoTour/GoTour.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"1b0587e8-5862-4808-85cb-d4d8f5683b9e"
+	.asciz	"5957e0a4-800a-4fc5-8803-64e81fe5dd79"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- GoTour/GoTour/MVVM/ViewModel/ToursViewModel.cs
+++ GoTour/GoTour/MVVM/ViewModel/ToursViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using GoTour.Core;
+using GoTour.Database;
+using GoTour.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace GoTour.MVVM.ViewModel
@@ -9,10 +13,56 @@ namespace GoTour.MVVM.ViewModel
     class ToursViewModel: ObservableObject
     {
         INavigation navigation;
+        FirebaseHelper firebaseHelper;
         public ToursViewModel() { }
         public ToursViewModel(INavigation navigation)
         {
             this.navigation = navigation;
+            firebaseHelper = new FirebaseHelper();
+            Places = new ObservableCollection<Place>();
+            Places2 = new ObservableCollection<Place>();
+            getPlacesAsync();
+        }
+
+        async Task getPlacesAsync()
+        {
+            //await firebaseHelper.AddPlace("3", "VietName", "VN ne", "https://i.pinimg.com/564x/5a/41/04/5a41046452cc2481693ce2df3c93fbc4.jpg");
+
+            List<Place> temp = await firebaseHelper.GetAllPlaces();
+
+            foreach (Place p in temp)
+            {
+                Places.Add(p);
+            }
+            foreach (Place p in temp)
+            {
+                Places2.Add(p);
+            }
+            int a = 5;
+        }
+
+        private ObservableCollection<Place> _places2;
+        public ObservableCollection<Place> Places2
+        {
+            get { return _places2; }
+            set
+            {
+                _places2 = value;
+                OnPropertyChanged("Places2");
+            }
+        }
+        private ObservableCollection<Place> _places;
+
+        public ObservableCollection<Place> Places
+        {
+            get { return _places; }
+            set
+            {
+                _places = value;
+                OnPropertyChanged("Places");
+            }
         }
     }
+
+   
 }
