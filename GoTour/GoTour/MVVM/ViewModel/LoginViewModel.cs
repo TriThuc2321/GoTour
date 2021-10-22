0eedc626099347182cb8aedf98c6144ed17d9cb4
TriThuc2321 <19522321@gm.uit.edu.vn>
2021-10-22 11:04:51 +0700

    login

---
+++ GoTour/GoTour/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,66 @@
+﻿using GoTour.Core;
+using GoTour.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GoTour.MVVM.ViewModel
+{
+    class LoginViewModel: ObservableObject
+    {
+        INavigation navigation;
+        public LoginViewModel() {}
+
+        public Command LoginCommand { get; }
+        public Command ForgotPasswordCommand { get; }
+        public Command RememberCommand { get; }
+        public LoginViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+            LoginCommand = new Command(loginHandle);
+            ForgotPasswordCommand = new Command(forgotHandle);
+        }
+
+        void loginHandle(object obj)
+        {
+            navigation.PushAsync(new HomeView());
+        }
+        void forgotHandle(object obj)
+        {
+            navigation.PushAsync(new HomeView());
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        private bool rememberAccount;
+        public bool RememberAccount
+        {
+            get { return rememberAccount; }
+            set
+            {
+                rememberAccount = value;
+                OnPropertyChanged("RememberAccount");
+            }
+        }
+
+    }
+}
