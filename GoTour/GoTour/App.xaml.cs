b3c9d6f163951d88cb87ef69e378e91b655e1c07
Linh Nguyen <72326769+khanhlinh1406@users.noreply.github.com>
2021-10-24 17:38:19 +0700

    ok

--- GoTour/GoTour/App.xaml.cs
+++ GoTour/GoTour/App.xaml.cs
@@ -11,7 +11,7 @@ namespace GoTour
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new FavoriteView());
+            MainPage = new NavigationPage(new LoginView());
         }
 
         protected override void OnStart()
