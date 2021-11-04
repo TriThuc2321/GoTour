671a5da1de6fc8944ed336707c3c2adfb849fa50
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2021-11-04 10:33:34 +0700

    Update User _ Done
    Updated by Huynh at 10:33 AM on Nov 11th

--- GoTour/GoTour.Android/Resources/Resource.designer.cs
+++ GoTour/GoTour.Android/Resources/Resource.designer.cs
@@ -12043,25 +12043,28 @@ namespace GoTour.Droid
 			public const int planeIcon = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int searchIcon = 2131165339;
+			public const int refreshIcon = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int test_custom_background = 2131165340;
+			public const int searchIcon = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int thuc = 2131165341;
+			public const int test_custom_background = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int thuc = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tourIcon = 2131165344;
+			public const int tooltip_frame_light = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int userOutlineIcon = 2131165345;
+			public const int tourIcon = 2131165345;
+			
+			// aapt resource value: 0x7F0700A2
+			public const int userOutlineIcon = 2131165346;
 			
 			static Drawable()
 			{
