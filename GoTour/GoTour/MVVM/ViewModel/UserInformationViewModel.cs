757cdeb56855482097d18860cf993662140a0a7b
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2021-11-10 20:08:52 +0700

    Profile User  Last commition
    DONE at 20:08 on 10/11/2021

--- GoTour/GoTour/MVVM/ViewModel/UserInformationViewModel.cs
+++ GoTour/GoTour/MVVM/ViewModel/UserInformationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Firebase.Database;
@@ -26,9 +27,14 @@ namespace GoTour.MVVM.ViewModel
             }
         }
         FirebaseClient firebase = new FirebaseClient("https://gotour-98c79-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+        //COMMAND
         public Command AddImageCommand { get; }
         public Command UpdateUserInfo { get; }
         public Command RefreshCommand { get; set; }
+        public Command EditTextCommand { get; }
+
+        //USER PROPERTIES
         public string CurrRank { get; set; }
         public string CurrScore { get; set; }
         private string name;
@@ -101,8 +107,12 @@ namespace GoTour.MVVM.ViewModel
                 OnPropertyChanged("ProfilePic");
             }
         }
+
+        //LIST USER
         public List<User> listUser = new List<User>();
 
+
+        //REFRESHING
         private bool isRefreshing;
         public bool IsRefreshing
         {
@@ -114,31 +124,95 @@ namespace GoTour.MVVM.ViewModel
             }
         }
 
+        //EDIT HANDLE ICON AND ISENABLE ENTRY
+        private string iconSource;
+        public string IconSource
+        {
+            get { return iconSource; }
+            set
+            {
+                iconSource = value;
+                OnPropertyChanged("IconSource");
+
+            }
+        }
+        private bool isEdit;
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set
+            {
+                isEdit = value;
+                OnPropertyChanged("IsEdit");
+
+            }
+        }
+
+
+        //CONSTRUCTOR
         public UserInformationViewModel() { }
         public UserInformationViewModel(INavigation navigation, Shell curentShell)
         {
             this.navigation = navigation;
             this.curentShell = curentShell;
             CurrUser = DataManager.Ins.CurrentUser;
+            Name = CurrUser.name;
             PutData();
+            IsEdit = false;
+            IconSource = "editIcon.png";
+            EditTextCommand = new Command(editTextHandle);
             UpdateUserInfo = new Command(updateUser);
             RefreshCommand = new Command(() => PutData());
-
             AddImageCommand = new Command(changePhoto);
         }
 
-       async void changePhoto(object obj)
+
+        //EDIT TEXT HANDLE
+        private void editTextHandle(object obj)
+        {           
+            if(IconSource.Equals("delete.png"))
+            {
+                if(!Name.Equals(DataManager.Ins.CurrentUser.name) || string.IsNullOrWhiteSpace(Name) ||
+                    !CMND.Equals(CurrUser.cmnd) || string.IsNullOrWhiteSpace(CMND) ||
+                    !Contact.Equals(CurrUser.contact) || string.IsNullOrWhiteSpace(Contact) ||
+                    !Birthday.Equals(CurrUser.birthday) || !Address.Equals(CurrUser.address) ||
+                    string.IsNullOrWhiteSpace(Address))
+                {
+                    PutData();
+                }
+                else
+                {
+                    IconSource = "editIcon.png";
+                    IsEdit = !IsEdit;
+                }
+            }
+            else
+            {
+                IconSource = "delete.png";
+                IsEdit = !IsEdit;
+            }
+        }
+
+        //CHANGE AVT HANDLE
+        async void changePhoto(object obj)
         {
-            
             await CrossMedia.Current.Initialize();
 
             var imgData = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions());
+            if(imgData == null)
+            {
+                return;
+            }
+            else
+            {
+                string url = await DataManager.Ins.UsersServices.saveImage(imgData.GetStream(), CurrUser.email, CurrUser.name);
 
-            string url = await DataManager.Ins.UsersServices.saveImage(imgData.GetStream(), CurrUser.email, CurrUser.name);
-
-            ProfilePic = url;
-            updateUser();
+                ProfilePic = url;
+                updateUser();
+            }
         }
+
+        //PUT DATA INTO CONTROL HANDLE
         async Task PutData()
         {
             IsRefreshing = true;
@@ -158,40 +232,61 @@ namespace GoTour.MVVM.ViewModel
                     ProfilePic = listUser[i].profilePic;
                 }
             }
+            IsEdit = false;
+            IconSource = "editIcon.png";
             IsRefreshing = false;
         }
+
+        //UPDATE DATABASE HANDLE
          async void updateUser()
         {
-            if ( Name == "" || Contact == "" || Address == "" || Birthday == "" || CMND == "")
+            if ( string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(Contact) || string.IsNullOrWhiteSpace(Address) || string.IsNullOrWhiteSpace(Birthday) || string.IsNullOrWhiteSpace(CMND))
             {
-                DependencyService.Get<IToast>().ShortToast("Please fill out your information");
+                DependencyService.Get<IToast>().ShortToast("Please fill out your information");              
             } else
             {
-                User user = new User { name = Name, address = Address, birthday = Birthday, cmnd = CMND, contact = Contact, email = Email, rank = CurrUser.rank, score = CurrUser.score, profilePic = ProfilePic, password = CurrUser.password };
-                var toUpdateUser = (await firebase
-             .Child("Users")
-             .OnceAsync<User>()).Where(a => a.Object.email == user.email).FirstOrDefault();
+                if(ContactValidation(Contact) == false)
+                {
+                    DependencyService.Get<IToast>().ShortToast("Your contact should have ten numerics and start with number 0");
+                }
+                else
+                {
+                    User user = new User { name = Name, address = Address, birthday = Birthday, cmnd = CMND, contact = Contact, email = Email, rank = CurrUser.rank, score = CurrUser.score, profilePic = ProfilePic, password = CurrUser.password };
+                    var toUpdateUser = (await firebase
+                 .Child("Users")
+                 .OnceAsync<User>()).Where(a => a.Object.email == user.email).FirstOrDefault();
 
-                await firebase
-                  .Child("Users")
-                  .Child(toUpdateUser.Key)
-                  .PutAsync(new User
-                  {
-                      email = user.email,
-                      password = user.password,
-                      name = user.name,
-                      contact = user.contact,
-                      birthday = user.birthday,
-                      cmnd = user.cmnd,
-                      profilePic = user.profilePic,
-                      address = user.address,
-                      score = user.score,
-                      rank = user.rank
-                  });
-                DependencyService.Get<IToast>().ShortToast("Saved your profile successfully");
-            }
-           
+                    await firebase
+                      .Child("Users")
+                      .Child(toUpdateUser.Key)
+                      .PutAsync(new User
+                      {
+                          email = user.email,
+                          password = user.password,
+                          name = user.name,
+                          contact = user.contact,
+                          birthday = user.birthday,
+                          cmnd = user.cmnd,
+                          profilePic = user.profilePic,
+                          address = user.address,
+                          score = user.score,
+                          rank = user.rank
+                      });
+                    DependencyService.Get<IToast>().ShortToast("Saved your profile successfully");
+                    IsEdit = false;
+                    IconSource = "editIcon.png";
+                }              
+            }  
         }
 
+        //CONTACT VALIDDATION
+        private bool ContactValidation(string contact)
+        {
+            if(contact.Length == 10 && contact[0].ToString() == "0")
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
