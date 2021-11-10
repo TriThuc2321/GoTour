b27a07eb64f45956bb39d46c7c95a4e4a4c01d43
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-11-10 11:07:19 +0700

    ok

--- GoTour/GoTour/MVVM/ViewModel/StayPlaceManagerViewModel.cs
+++ GoTour/GoTour/MVVM/ViewModel/StayPlaceManagerViewModel.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using GoTour.Core;
+using GoTour.Database;
+using GoTour.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace GoTour.MVVM.ViewModel
 {
-    public class StayPlaceManagerViewModel
+    public class StayPlaceManagerViewModel : ObservableObject
     {
+        INavigation navigation;
+        Shell currentShell;
+        public StayPlaceManagerViewModel() { }
+        public StayPlaceManagerViewModel(INavigation navigation, Shell currentShell)
+        {
+            this.navigation = navigation;
+            this.currentShell = currentShell;
+
+            ListStayPlace = DataManager.Ins.ListStayPlace;
+        }
+
+        private StayPlace selectedStayPlace;
+        public StayPlace SelectedStayPlace
+        {
+            get { return selectedStayPlace; }
+            set
+            {
+                selectedStayPlace = value;
+                OnPropertyChanged("SelectedStayPlace");
+
+            }
+        }
+        private ObservableCollection<StayPlace> listStayPlace;
+        public ObservableCollection<StayPlace> ListStayPlace
+        {
+            get { return listStayPlace; }
+            set
+            {
+                listStayPlace = value;
+                OnPropertyChanged("ListStayPlace");
+            }
+        }
     }
 }
