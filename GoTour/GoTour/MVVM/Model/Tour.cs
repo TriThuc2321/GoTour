d2a74e041a4c89275c3a8c4f27372a6aa1e5826a
HuynhVong <71302943+HuynhVong@users.noreply.github.com>
2021-11-17 15:50:44 +0700

    huhu

--- GoTour/GoTour/MVVM/Model/Tour.cs
+++ GoTour/GoTour/MVVM/Model/Tour.cs
@@ -21,9 +21,11 @@ namespace GoTour.MVVM.Model
         public string remaining { get; set; }
         public List<PlaceId_StayPlace> SPforPList { get; set; }
         public List<PlaceId_Duration> placeDurationList { get; set; }
+        public List<Review> reviewList { get; set; }
+        public string starNumber { get; set; }
         public Tour() { }
 
-        public Tour(string id, string name, List<string> imgSource, string startTime, string duration, List<PlaceId_StayPlace> SPforPList ,List<string> tourGuide, string passengerNumber, string description, bool isOccured, string basePrice, string remaining)
+        public Tour(string id, string name, List<string> imgSource, string startTime, string duration, List<string> tourGuide, string passengerNumber, string description, bool isOccured, string basePrice, string remaining, List<PlaceId_StayPlace> sPforPList, List<PlaceId_Duration> placeDurationList, List<Review> reviewList, string starNumber)
         {
             this.id = id;
             this.name = name;
@@ -34,10 +36,12 @@ namespace GoTour.MVVM.Model
             this.passengerNumber = passengerNumber;
             this.description = description;
             this.isOccured = isOccured;
-            this.placeDurationList = new List<PlaceId_Duration>();
             this.basePrice = basePrice;
-            this.SPforPList = SPforPList;
             this.remaining = remaining;
+            SPforPList = sPforPList;
+            this.placeDurationList = placeDurationList;
+            this.reviewList = reviewList;
+            this.starNumber = starNumber;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -46,19 +50,6 @@ namespace GoTour.MVVM.Model
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        //public int CompareTo(Tour other)
-        //{
-        //    if (Int32.Parse(this.basePrice).CompareTo(Int32.Parse(other.basePrice)) == 0)
-        //    {
-        //        if (Int32.Parse(basePrice) > Int32.Parse(other.basePrice))
-        //            return 1; //Age lớn hơn 
-        //        if (Int32.Parse(basePrice) < Int32.Parse(other.basePrice))
-        //            return -1;  //Age nhỏ hơn
-        //        if (Int32.Parse(basePrice) == Int32.Parse(other.basePrice))
-        //            return 0; //Hai đối tượng coi như bằng nhau
-        //    }
-        //    return this.basePrice.CompareTo(other.basePrice);
-        //}
 
     }
 }
