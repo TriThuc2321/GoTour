f4e6e9a07c3dfea2d51e7accd78c17b7d6346dfb
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-11-17 10:05:55 +0700

    ok

--- GoTour/GoTour/MVVM/ViewModel/ConfirmInvoiceViewModel.cs
+++ GoTour/GoTour/MVVM/ViewModel/ConfirmInvoiceViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using GoTour.Core;
+using GoTour.Database;
+using GoTour.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 
@@ -15,8 +18,65 @@ namespace GoTour.MVVM.ViewModel
         public ConfirmInvoiceViewModel(INavigation navigation, Shell currentShell)
         {
             this.navigation = navigation;
-            this.currentShell = currentShell;          
+            this.currentShell = currentShell;
+            InitList();
+            
 
         }
+        private void InitList()
+        {
+            ListTour = new List<Tour>();
+            ListInvoice = new List<Invoice>();
+            ListBookedSticket = DataManager.Ins.ListBookedTickets;
+            foreach (Tour ite in DataManager.Ins.ListTour)
+            {
+                listTour.Add(ite);
+            }
+            foreach (Invoice ite1 in DataManager.Ins.ListInvoice)
+            {
+                listInvoice.Add(ite1);
+            }
+            foreach (BookedTicket ite in ListBookedSticket)
+            {
+                ite.tour = listTour.Find(e => e.id == ite.tour.id);
+                ite.invoice = listInvoice.Find(e => e.id == ite.invoice.id);
+                if (ite.invoice.isPaid == true) ListBookedSticket.Remove(ite);
+            }
+           
+        }
+
+        private ObservableCollection<BookedTicket> listBookedSticket;
+        public ObservableCollection<BookedTicket> ListBookedSticket
+        {
+            get { return listBookedSticket; }
+            set
+            {
+                listBookedSticket = value;
+                OnPropertyChanged("ListBookedSticket");
+            }
+        }
+        private List<Tour> listTour;
+        public List<Tour> ListTour
+        {
+            get { return listTour; }
+            set
+            {
+                listTour = value;
+                OnPropertyChanged("ListTour");
+            }
+        }
+        private List<Invoice> listInvoice;
+        public List<Invoice> ListInvoice
+        {
+            get { return listInvoice; }
+            set
+            {
+                listInvoice = value;
+                OnPropertyChanged("ListInvoice");
+            }
+        }
+
     }
+
+   
 }
